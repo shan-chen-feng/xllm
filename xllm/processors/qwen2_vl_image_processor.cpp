49809930297bc65a0c015912bc5a17080d0e8010
shan-chen-feng <shanchenfeng@h-partners.com>
2026-01-09 20:04:27 +0800

    refactor

--- xllm/processors/qwen2_vl_image_processor.cpp
+++ xllm/processors/qwen2_vl_image_processor.cpp
@@ -163,17 +163,19 @@ bool Qwen2VLImageProcessor::process(const MMInput& inputs, MMData& datas) {
     std::vector<torch::Tensor> videos;
     std::vector<VideoMetadata> video_meta_list;
 
-    if (input_item.type_ == MMType::IMAGE) {
-      if (input_item.decode_data_.defined()) {
-        images.push_back(input_item.decode_data_);
+    if (input_item.type_ & MMType::IMAGE) {
+      if (input_item.decode_image_.defined()) {
+        images.push_back(input_item.decode_image_);
       } else if (input_item.embedding_.embedding.defined()) {
         images_embedding.push_back(input_item.embedding_);
       }
-    } else if (input_item.type_ == MMType::VIDEO) {
-      if (input_item.decode_data_.defined()) {
-        videos.push_back(input_item.decode_data_);
+    } else if (input_item.type_ & MMType::VIDEO) {
+      if (input_item.decode_video_.defined()) {
+        videos.push_back(input_item.decode_video_);
       }
       video_meta_list.push_back(input_item.video_meta_);
+    } else if (input_item.type_ & MMType::AUDIO) {
+      continue;
     }
 
     if (images_embedding.empty() && images.empty() &&
