80593bf9aed6a9e519a116d767910ce4d0ecfbee
shan-chen-feng <shanchenfeng@h-partners.com>
2026-06-29 11:46:43 +0800

    feat: temp support draft eagle3.

--- xllm/core/runtime/eagle3_worker_impl.cpp
+++ xllm/core/runtime/eagle3_worker_impl.cpp
@@ -39,7 +39,7 @@ runtime::Options eagle3_draft_options(const runtime::Options& options) {
       .is_draft_engine(true)
       .num_decoding_tokens(1)
       .num_speculative_tokens(0)
-      .enable_graph_aux_hidden_states(false)
+      .enable_graph_aux_hidden_states(options.enable_graph())
       .backend("llm");
   return opts;
 }
